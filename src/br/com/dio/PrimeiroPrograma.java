46b337e0ed597d47c47d9cc234cd2eded520c021
robson <robsonfs.sp@hotmail.com>
2022-06-25 17:37:42 -0300

    Includ class Livros

--- src/br/com/dio/PrimeiroPrograma.java
+++ src/br/com/dio/PrimeiroPrograma.java
@@ -4,13 +4,32 @@ import br.com.dio.model.Gato;
 
 public class PrimeiroPrograma {
     public static void main(String[] args) {
-        Gato gato = new Gato();
 
-        System.out.println(gato);
+        Gato gato1 = new Gato();
+//        Livros livro = new Livros();
 
-//        int a = 5;
-//        int b = 3;
-//        System.out.println("Hello World! " + (a + b));
+        int a = 2;
+        int b = 3;
+
+        System.out.println("Hello World!" + (a + b));
+
+        System.out.println(gato1);
+//        System.out.println(livro);
     }
+
+/*    class Livros {
+        private String nome;
+        private String npag;
+
+        public Livros() {
+        }
+
+        public Livros(String nome, String npag) {
+            super();
+            this.nome = nome;
+            this.npag = npag;
+        };
+
+    }*/
 
 }
