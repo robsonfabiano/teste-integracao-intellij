722ad5fc3c86d10df23c2bd8990b5c6a66d34c64
robson <robsonfs.sp@hotmail.com>
2022-06-25 18:38:25 -0300

    Include class Livros

--- src/br/com/dio/PrimeiroPrograma.java
+++ src/br/com/dio/PrimeiroPrograma.java
@@ -6,7 +6,8 @@ public class PrimeiroPrograma {
     public static void main(String[] args) {
 
         Gato gato1 = new Gato();
-//        Livros livro = new Livros();
+        Livros livro = new Livros("O problema dos três copos", 300);
+
 
         int a = 2;
         int b = 3;
@@ -14,22 +15,47 @@ public class PrimeiroPrograma {
         System.out.println("Hello World!" + (a + b));
 
         System.out.println(gato1);
-//        System.out.println(livro);
+        System.out.println(livro);
     }
 
-/*    class Livros {
-        private String nome;
-        private String npag;
+}
 
-        public Livros() {
-        }
+class Livros {
+    private String nome;
+    private Integer npag;
 
-        public Livros(String nome, String npag) {
-            super();
-            this.nome = nome;
-            this.npag = npag;
-        };
+    public Livros() {
+    }
 
-    }*/
+    public Livros(String nome, Integer npag) {
+        super();
+        this.nome = nome;
+        this.npag = npag;
+    }
 
+    
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Integer getNpag() {
+        return npag;
+    }
+
+    public void setNpag(Integer npag) {
+        this.npag = npag;
+    }
+
+    @Override
+    public String toString() {
+        return "Livros{" +
+                "nome='" + nome + '\'' +
+                ", npag='" + npag + '\'' +
+                '}';
+    }
 }
